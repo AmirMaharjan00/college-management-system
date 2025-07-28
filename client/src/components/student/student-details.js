bc625e55127a40117c17fe394fedf747bd015151
Saroj Maharjan <sarojmhzn28@gmail.com>
2025-07-28 18:15:24 +0545

    student

--- client/src/components/student/student-details.js
+++ client/src/components/student/student-details.js
@@ -4,7 +4,7 @@ import { ourFetch } from '../functions'
 import { Link } from 'react-router-dom'
 import { useDate } from '../includes/hooks'
 import { TodaysDate } from '../includes/components-hooks'
-import './profile.scss'
+import './student-details.scss'
 
 
 /**
@@ -30,94 +30,109 @@ export const StudentDetails = () => {
             <TodaysDate />
         </div>
 
-        <div className="personal-info-wrapper">
-            {/* Left Section */}
-            {/* <div className="left-panel">
-                <h3>Personal Information</h3>
-                <div className="profile-section">
-                {image ? (
-                    <img src="#" alt="Profile" className="profile-img" />
-                ) : (
-                    <div className="upload-placeholder">
-                    <label htmlFor="upload-input">Click to Upload</label>
-                    <input
-                        type="file"
-                        id="upload-input"
-                        accept=""
-                        onChange={handleImageChange}
-                        hidden
-                    />
-                    <p>or drag and drop</p>
-                    <small>JPG or PNG (Max 450 x 450 px)</small>
+        <div className='student-body'>
+                <div className="student-sidebar">
+                    <div>
+                        <div>
+                            <figure></figure>
+                            <div>
+                                <span>Active</span>
+                                <h2>Janet Daniel</h2>
+                                <p>AD123456</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h3>Basic Information</h3>
+                            <div>
+                                <p>
+                                    <span>Roll No</span>
+                                    <span>35013</span>
+                                </p>
+                                <p>
+                                    <span>Gender</span>
+                                    <span>Female</span>
+                                </p>
+                                <p>
+                                    <span>Date Of Birth</span>
+                                    <span>25 Jan 2008</span>
+                                </p>
+                                <p>
+                                    <span>Mother Tongue</span>
+                                    <span>Nepali</span>
+                                </p>
+                                <p>
+                                    <span>Language</span>
+                                    <span>Nepali</span>
+                                </p>
+                            </div>
+                            <button>Add Fees</button>
+                        </div>
                     </div>
-                )}
-                {image && (
-                    <div className="image-actions">
-                    <button onClick={handleDelete}>Delete</button>
-                    <label htmlFor="upload-input" className="update-btn">Update</label>
+                    <div>
+                        <h3>Primary Contact Info</h3>
+                        <div>
+                            <span className="icon"></span>
+                            <div>
+                                <p>Phone Number</p>
+                                <p>+ 1 324324</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="icon"></span>
+                            <div>
+                                <p>Phone Number</p>
+                                <p>+ 1 324324</p>
+                            </div>
+                        </div>
                     </div>
-                )}
-                </div>
-            </div> */}
 
-            {/* Right Section */}
-            <div className="right-panel">
-                <div className="header">
-                <h3>Personal Information</h3>
-                <button className="edit-btn">Edit</button>
+                    <div>
+                        <h3>Sibiling Information</h3>
+                        <div>
+                            <div className="thumb">
+                                <figure></figure>
+                            </div>
+                            <div>
+                                <p>Ralph Claudia</p>
+                                <span>III, B</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="thumb">
+                                <figure></figure>
+                            </div>
+                            <div>
+                                <p>Julie Scott</p>
+                                <span>V, A</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
                 </div>
-                <div className="form-fields">
-                <div className="row">
-                    <label>First Name</label>
-                    <input type="text" placeholder="Enter First Name" />
-                    <label>Last Name</label>
-                    <input type="text" placeholder="Enter Last Name" />
-                </div>
-                <label>Email Address</label>
-                <input type="email" placeholder="Enter Email" />
-                <div className="row">
-                    <label>User Name</label>
-                    <input type="text" placeholder="Enter User Name" />
-                    <label>Phone Number</label>
-                    <input type="tel" placeholder="Enter Phone Number" />
-                </div>
+
+                <div className="student-main">
+                    <div className="student-tab">
+                        <ul>
+                            <li>Student Details</li>
+                            <li>Time Table</li>
+                            <li>Leave & Attendance</li>
+                            <li>Fees</li>
+                            <li>Exam & Results</li>
+                            <li>Library</li>
+                        </ul>
+                    </div>
+                    <div className="personal-information">
+                        <h3>Parents Information</h3>
+                        <div>
+                            <div className="parents">
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div className="address-info-wrapper">
-                {/* Address Info */}
-                <div className="card">
-                    <div className="card-header">
-                    <h3>Address Information</h3>
-                    <button className="edit-btn">Edit</button>
-                    </div>
-                    <div className="form-fields">
-                    <input type="text" placeholder="Enter Address" />
-                    <div className="row">
-                        <input type="text" placeholder="Enter Country" />
-                        <input type="text" placeholder="Enter State" />
-                    </div>
-                    <div className="row">
-                        <input type="text" placeholder="City" />
-                        <input type="text" placeholder="Enter Postal Code" />
-                    </div>
-                    </div>
-                </div>
-
-                {/* Password Section */}
-                <div className="card">
-                    <div className="card-header">
-                    <h3>Password</h3>
-                    <button className="change-btn">Change</button>
-                    </div>
-                    <div className="form-fields">
-                    <div className="password-input">
-                        <input type="password" placeholder="Current Password" />
-                        <span className="toggle-icon">👁️‍🗨️</span>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </main>
 }
