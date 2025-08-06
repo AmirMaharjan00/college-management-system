7144e7156d761458a2a792ab8f8ee50c97eedbed
Amir Maharjan <mhrznamir.am@gmail.com>
2025-08-06 08:16:22 +0545

    starging examinations

--- client/src/components/academic/examinations.js
+++ client/src/components/academic/examinations.js
@@ -1,8 +1,31 @@
+import { useEffect, useState, useMemo, useContext } from "react"
+import { Breadcrumb, Pagination, RowAndSearch, ActionButton } from "../components"
+import { fetchCallback, ourFetch, getScript } from "../functions"
+import { useDate } from "../includes/hooks"
+import '../assets/scss/table.scss'
+import '../assets/scss/academic.scss'
+import { AddNewCourseSubject } from '../forms/add-new-cs'
+import { GLOBALCONTEXT } from "../../App"
+import { TodaysDate } from "../includes/components-hooks"
+
+
 /**
  * Examinations
  */
 export const Examinations = () => {
     return <main className="cmg-main" id="cmg-main">
-        Examinations
+        
+        <div className='page-header'>
+
+            <Breadcrumb
+                headLabel = 'Examination'
+                currentPageLabel = 'Examination'
+                middle = { false }
+            />
+
+            <TodaysDate />
+
+        </div>
+
     </main>
 }
