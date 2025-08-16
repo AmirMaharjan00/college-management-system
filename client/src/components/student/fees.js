3b0611f1cea5a8d1ebe8a56ac0a89a7ecb1c44cb
Amir Maharjan <mhrznamir.am@gmail.com>
2025-08-16 18:36:40 +0545

    fees update

--- client/src/components/student/fees.js
+++ client/src/components/student/fees.js
@@ -72,11 +72,13 @@ export const StudentFees = () => {
  * 
  * @since 1.0.0
  */
-export const PayFees = () => {
+export const PayFees = ( props ) => {
 	const Global = useContext( GLOBALCONTEXT ),
-		{ loggedInUser } = Global,
+		{ loggedInUser, feeDetails, setFeeDetails } = Global,
+		{ includeSelect } = props,
 		{ role, id: userID, name, courseId, semester: _thisSem } = loggedInUser,
 		[ students, setStudents ] = useState([]),
+		[ priceInWords, setPriceInWords ] = useState( '' ),
 		studentOptions = useMemo(() => {
 			return students.reduce(( val, student ) => {
 				let { id, name } = student
@@ -88,27 +90,25 @@ export const PayFees = () => {
 			if( role === 'admin' ) return true
 			return false
 		}, [ role ]),
-		[formData, setFormData] = useState({
-			studentName: isAdmin ? '' : name,
-			studentId: isAdmin ? '' : userID,
-			course: isAdmin ? '' : courseId,
-			semester: isAdmin ? '' : _thisSem,
-			feeType: '',
-			paymentMethod: '',
-			price: '',
-			priceInWords: '',
-			remarks: '',
-			successUrl: 'https://developer.esewa.com.np/success',
-			failureUrl: 'https://developer.esewa.com.np/failure',
+		{ userId, amount, message, type, purpose, paymentMethod } = feeDetails,
+		[ formData, setFormData ] = useState({
+			tax_amount: 0,
+			total_amount: amount,
+			transaction_uuid: Math.floor(100000 + Math.random() * 900000).toString(),
+			product_code: "EPAYTEST",
+			product_service_charge: 0,
+			product_delivery_charge: 0,
+			success_url: 'http://localhost:5000/digital-payment/success',
+			failure_url: 'http://localhost:5000/digital-payment/failure',
+			signed_field_names: 'total_amount,transaction_uuid,product_code'
 		}),
-		{ studentName, studentId, program, semester, feeType, paymentMethod, price, priceInWords, remarks, successUrl, failureUrl } = formData,
-		transactionUuid = Math.floor(100000 + Math.random() * 900000).toString(),
+		{ tax_amount, total_amount, transaction_uuid, product_code, product_service_charge, product_delivery_charge, success_url, failure_url, signed_field_names } = formData,
 		SECRET_KEY = '8gBm/:&EnhH.1/q',
 		signature = useMemo(() => {
-			const stringToSign = `total_amount=${price},transaction_uuid=${transactionUuid},product_code=EPAYTEST`,
+			const stringToSign = `total_amount=${amount},transaction_uuid=${transaction_uuid},product_code=EPAYTEST`,
 				rawSignature = CryptoJS.HmacSHA256(stringToSign, SECRET_KEY );
 			return CryptoJS.enc.Base64.stringify(rawSignature);
-		}, [ price, transactionUuid ] )
+		}, [ amount, transaction_uuid ] )
 
 	useEffect(() => {
 		if( isAdmin ) {
@@ -120,29 +120,33 @@ export const PayFees = () => {
 		}
 	}, [])
 
+	useEffect(() => {
+		setPriceInWords( toWords(amount || 0).toUpperCase() + ' ONLY' )
+	}, [ amount ])
+
 	const handleChange = ( e ) => {
 		let name = e.target.name,
 			value = e.target.value
 
-		setFormData({ 
-			...formData,
+		setFeeDetails({ 
+			...feeDetails,
 			[ name ]: value
 		});
 	};
 
 	const handlePriceChange = (e) => {
-		const price = e.target.value;
-		setFormData((prev) => ({
+		const amount = e.target.value;
+		setFeeDetails((prev) => ({
 			...prev,
-			price,
-			priceInWords: toWords(price || 0).toUpperCase() + ' ONLY'
+			amount,
+			priceInWords: toWords(amount || 0).toUpperCase() + ' ONLY'
 		}));
 	};
 
 	const handleReactSelectChange = ( option ) => {
-		setFormData({
-			...formData,
-			studentId: option.value
+		setFeeDetails({
+			...feeDetails,
+			userId: option.value
 		})
 	}
 
@@ -153,51 +157,50 @@ export const PayFees = () => {
 					<h2 className="form-title">College Fee Payment</h2>
 					<span className="form-excerpt">Please fill in your fee details below.</span>
 				</div>
-				{ isAdmin && <div className="form-field">
+				{ includeSelect && <div className="form-field">
 					<label className="form-label" htmlFor="feeType">Name <span className="form-error">*</span></label>
 					<Select
 						options = { studentOptions }
 						className = 'react-select'
-						name = 'studentId'
-						value = { getCurrentSelectValue( studentOptions, studentId ) }
+						name = 'userId'
+						value = { getCurrentSelectValue( studentOptions, userId ) }
 						onChange = { handleReactSelectChange }
 					/>
 				</div> }
 
 				<div className="form-field">
-					<label className="form-label" htmlFor="price">Price <span className="form-error">*</span></label>
-					<input required type="number" id="price" name="price" min="0" value={ price } onChange={handlePriceChange} />
+					<label className="form-label" htmlFor="amount">Price <span className="form-error">*</span></label>
+					<input required type="number" id="amount" name="amount" min="0" value={ amount } onChange={handlePriceChange} />
 				</div>
 				<div className="form-field">
 					<label className="form-label" htmlFor="priceInWords">Price in Words <span className="form-error">*</span></label>
-					<input required type="text" id="priceInWords" name="priceInWords" value={formData.priceInWords} onChange={handleChange} readOnly />
+					<input required type="text" id="priceInWords" name="priceInWords" value={ priceInWords } onChange={handleChange} readOnly />
 				</div>
 
 				<div className="form-field">
-					<label className="form-label" htmlFor="remarks">Remarks (Optional)</label>
-					<textarea id="remarks" name="remarks" value={formData.remarks} onChange={handleChange}></textarea>
+					<label className="form-label" htmlFor="message">Remarks (Optional)</label>
+					<textarea id="message" name="message" value={ message } onChange={handleChange}></textarea>
 				</div>
 
 				<div className="form-field">
 					<label className="form-label">Payment Method <span className="form-error">*</span></label>
-					<select name='paymentMethod' id='paymentMethod' value={formData.paymentMethod} onChange={handleChange} required>
+					<select name='paymentMethod' id='paymentMethod' value={ paymentMethod } onChange={handleChange} required>
 						<option value=''>Select</option>
 						<option value='esewa'>eSewa</option>
 						<option value='khalti'>Khalti</option>
 						<option value='bank'>Bank Transfer</option>
 					</select>
 				</div>
-				<input type="hidden" name="amount" value={ '' } required />
-				<input type="hidden" name="tax_amount" value="0" required />
-				<input type="hidden" name="total_amount" value={''} required />
-				<input type="hidden" name="transaction_uuid" value={''} required />
-				<input type="hidden" name="product_code" value="EPAYTEST" required />
-				<input type="hidden" name="product_service_charge" value="0" required />
-				<input type="hidden" name="product_delivery_charge" value="0" required />
-				<input type="hidden" name="success_url" value="https://developer.esewa.com.np/success" required />
-				<input type="hidden" name="failure_url" value="https://developer.esewa.com.np/failure" required />
-				<input type="hidden" name="signed_field_names" value="total_amount,transaction_uuid,product_code" required />
-				<input type="hidden" name="signature" value={ '' } required />
+				<input type="hidden" name="tax_amount" value={ tax_amount } required />
+				<input type="hidden" name="total_amount" value={ total_amount } required />
+				<input type="hidden" name="transaction_uuid" value={ transaction_uuid } required />
+				<input type="hidden" name="product_code" value={ product_code } required />
+				<input type="hidden" name="product_service_charge" value={ product_service_charge } required />
+				<input type="hidden" name="product_delivery_charge" value={ product_delivery_charge } required />
+				<input type="hidden" name="success_url" value={ success_url } required />
+				<input type="hidden" name="failure_url" value={ failure_url } required />
+				<input type="hidden" name="signed_field_names" value={ signed_field_names } required />
+				<input type="hidden" name="signature" value={ signature } required />
 				<input value="Pay Now" className="submit-button" type="submit"/>
 			</form>
 		</div>
